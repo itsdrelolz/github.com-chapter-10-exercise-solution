22446697bf3d55ea94e202cd3d9bfbf21c2e33a5
itsdrelolz <harperandre277@gmail.com>
2025-04-03 16:05:48 -0400

    modified function to use generic Number type

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/chapter-10-exercise/solution
 
-go 1.22.2
+go 1.23.0
+
+toolchain go1.23.8
+
+require golang.org/x/exp v0.0.0-20250305212735-054e65f0b394 // indirect
--- solution.go
+++ solution.go
@@ -1,8 +1,14 @@
 // Package ch10_solution contains the solution to the exercises in chapter 10
 package ch10_solution
 
-// Add function takes in two parameters of type int and returns their sum.
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add function takes in two parameters of type Number and returns their sum.
 // It follows the rules specified by https://www.mathsisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
