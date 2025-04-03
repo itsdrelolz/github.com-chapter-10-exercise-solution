2e4cec8f2aee5dff8bdeb90c06c0b81d3d4ce477
itsdrelolz <harperandre277@gmail.com>
2025-04-03 15:59:56 -0400

    added godoc comments

--- solution.go
+++ solution.go
@@ -1,5 +1,8 @@
-package main
+// Package ch10_solution contains the solution to the exercises in chapter 10
+package ch10_solution
 
+// Add function takes in two parameters of type int and returns their sum.
+// It follows the rules specified by https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
